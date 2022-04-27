ee287d76ac9db2ea5a017ac56f252e39ef374cbb
ChristineWasike <christinewasike22@gmail.com>
2022-04-27 11:45:03 +0300

    Search and Logout combined on home screen

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,34 +1,44 @@
 import 'package:movie_mate/service/auth.dart';
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+import '../search/search.dart';
 
+class Home extends StatelessWidget {
   final AuthService _auth = AuthService();
+
+  Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text('MATE',
-            style: TextStyle(color: Colors.black,
-            fontWeight: FontWeight.bold),
-
-          ),
-
-          backgroundColor: Colors.grey,
-          elevation: 0.0,
-          actions: <Widget>[
-            FlatButton.icon(
-              icon: Icon(Icons.person),
-              label: Text('Logout'),
-              onPressed: () async {
-                await _auth.signOut();
-              },
-            ),
-          ],
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text(
+          'MATE',
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
         ),
+        backgroundColor: Colors.grey,
+        elevation: 0.0,
+        actions: <Widget>[
+          FlatButton.icon(
+            icon: const Icon(Icons.person),
+            label: const Text('Logout'),
+            onPressed: () async {
+              await _auth.signOut();
+            },
+          ),
+        ],
+      ),
+      body: Align(
+        alignment: Alignment.topRight,
+        child: IconButton(
+            icon: const Icon(
+              Icons.search,
+              color: Colors.black,
+            ),
+            onPressed: () async {
+              showSearch(context: context, delegate: MovieSearch());
+            }),
       ),
     );
   }
