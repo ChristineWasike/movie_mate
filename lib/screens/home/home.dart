971c314d0e56a57a462e21f595481fa304d2369c
ChristineWasike <christinewasike22@gmail.com>
2022-04-29 22:53:48 +0300

    Added the teabs to the homesecreen

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -9,13 +9,15 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with TickerProviderStateMixin {
   String? uid = '';
+  late TabController _tabController;
 
   @override
   void initState() {
     getuid();
     super.initState();
+    _tabController = TabController(length: 2, vsync: this);
   }
 
   getuid() async {
@@ -41,7 +43,18 @@ class _HomeState extends State<Home> {
           'MATE',
           style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
         ),
-        backgroundColor: Colors.grey,
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: const <Widget>[
+            Tab(
+              icon: Icon(Icons.home),
+            ),
+            Tab(
+              icon: Icon(Icons.people),
+            ),
+          ],
+        ),
+        backgroundColor: const Color.fromARGB(179, 78, 78, 78),
         elevation: 0.0,
         actions: <Widget>[
           TextButton.icon(
@@ -53,53 +66,64 @@ class _HomeState extends State<Home> {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Align(
-            alignment: Alignment.topRight,
-            child: IconButton(
-                icon: const Icon(
-                  Icons.search,
-                  color: Colors.black,
-                ),
-                onPressed: () async {
-                  showSearch(context: context, delegate: MovieSearch());
-                }),
-          ),
-          Text('Your favorite movies will be found below',
-              style:
-                  TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
-          Expanded(
-            child: SizedBox(
-              child: Container(
-                child: StreamBuilder<QuerySnapshot>(
-                    stream: FirebaseFirestore.instance
-                        .collection('Favorites')
-                        .doc(uid == '' ? ' ' : uid)
-                        .collection('myfavorites')
-                        .snapshots(),
-                    builder: (context, AsyncSnapshot snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting ||
-                          snapshot.data == null) {
-                        return const Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      } else {
-                        print(uid);
-                        final docs = snapshot.data.docs;
-                        return ListView.builder(
-                            itemCount: docs.length,
-                            itemBuilder: (context, index) {
-                              return Container(
-                                child: Column(
-                                  children: [Text(docs[index]['movieName'])],
-                                ),
-                              );
-                            });
-                      }
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          Column(
+            children: [
+              Align(
+                alignment: Alignment.topRight,
+                child: IconButton(
+                    icon: const Icon(
+                      Icons.search,
+                      color: Colors.black,
+                    ),
+                    onPressed: () async {
+                      showSearch(context: context, delegate: MovieSearch());
                     }),
               ),
-            ),
+              const Text('Your favorite movies will be found below',
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.bold)),
+              Expanded(
+                child: SizedBox(
+                  child: Container(
+                    child: StreamBuilder<QuerySnapshot>(
+                        stream: FirebaseFirestore.instance
+                            .collection('Favorites')
+                            .doc(uid == '' ? ' ' : uid)
+                            .collection('myfavorites')
+                            .snapshots(),
+                        builder: (context, AsyncSnapshot snapshot) {
+                          if (snapshot.connectionState ==
+                                  ConnectionState.waiting ||
+                              snapshot.data == null) {
+                            return const Center(
+                              child: CircularProgressIndicator(),
+                            );
+                          } else {
+                            print(uid);
+                            final docs = snapshot.data.docs;
+                            return ListView.builder(
+                                itemCount: docs.length,
+                                itemBuilder: (context, index) {
+                                  return Container(
+                                    child: Column(
+                                      children: [
+                                        Text(docs[index]['movieName'])
+                                      ],
+                                    ),
+                                  );
+                                });
+                          }
+                        }),
+                  ),
+                ),
+              )
+            ],
+          ),
+          const Center(
+            child: Text("Social"),
           )
         ],
       ),
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -12,7 +12,7 @@ class Wrapper extends StatelessWidget {
   Widget build(BuildContext context) {
     final user = Provider.of<Users?>(context);
     if (user == null){
-      return Authenticate();
+      return const Authenticate();
     }
     else{
       return Home();
